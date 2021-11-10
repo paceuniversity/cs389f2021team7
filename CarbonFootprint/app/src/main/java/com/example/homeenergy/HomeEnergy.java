f7a1fc8965505a3b78606b4d10371ace8b436ebd
PatrickDwyer123 <pd83391p@pace.edu>
2021-11-09 20:59:54 -0500

    Changes to app

--- CarbonFootprint/app/src/main/java/com/example/homeenergy/HomeEnergy.java
+++ CarbonFootprint/app/src/main/java/com/example/homeenergy/HomeEnergy.java
@@ -2,12 +2,13 @@ package com.example.homeenergy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class HomeEnergy extends AppCompatActivity {
@@ -15,6 +16,11 @@ double naturalGas;
 double electricity;
 double fuelOil;
 double propane;
+Spinner primaryHeatSpinner;
+    public static final String NATURAL = "com.example.homeenergy.NATURAL";
+    public static final String ELECTRICITY = "com.example.homeenergy.ELECTRICITY";
+    public static final String FUELOIL = "com.example.homeenergy.FUELOIL";
+    public static final String PROPANE = "com.example.homeenergy.PROPANE";
 
 EditText naturalGasInput, electricityInput, fuelOilInput, propaneInput;
 
@@ -29,31 +35,51 @@ Button submit;
         electricityInput = (EditText) findViewById(R.id.electricityInput);
         fuelOilInput = (EditText)  findViewById(R.id.fuelOilInput);
         propaneInput = (EditText) findViewById(R.id.propaneInput);
+        primaryHeatSpinner = findViewById(R.id.primaryHeatSpinner);
+        //Intent intent = getIntent();
+        //int numberOfPeople = intent.getIntExtra(InitiateCalculator.HOUSEHOLDNUMBER, 0);
+
 
         submit = (Button) findViewById(R.id.estimatedC02);
+        primaryHeatSource();
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                naturalGas = Integer.valueOf(naturalGasInput.getText().toString());
-                electricity = Integer.valueOf(electricityInput.getText().toString());
-                fuelOil = Integer.valueOf(fuelOilInput.getText().toString());
-                propane = Integer.valueOf(propaneInput.getText().toString());
+                String primaryHeat = primaryHeatSpinner.getSelectedItem().toString();
+                if(primaryHeat.equals("Natural Gas") || primaryHeat.equals("Electricity") || primaryHeat.equals("Fuel Oil") || primaryHeat.equals("Propane")) {
+                    naturalGas = Integer.valueOf(naturalGasInput.getText().toString());
+                    electricity = Integer.valueOf(electricityInput.getText().toString());
+                    fuelOil = Integer.valueOf(fuelOilInput.getText().toString());
+                    propane = Integer.valueOf(propaneInput.getText().toString());
 
-                naturalGas = naturalGas * 11.2 * 12;
-                electricity = electricity * 4.6 * 12;
-                fuelOil = fuelOil * 5.6 * 12;
-                propane = propane * 5 * 12;
+                    naturalGas = naturalGas * 11.2 * 12;
+                    electricity = electricity * 4.6 * 12;
+                    fuelOil = fuelOil * 5.6 * 12;
+                    propane = propane * 5 * 12;
 
-                showToast(String.valueOf(naturalGas));
-                showToast(String.valueOf(electricity));
-                showToast(String.valueOf(fuelOil));
-                showToast(String.valueOf(propane));
+
+                    Intent intent = new Intent(HomeEnergy.this, HomeEnergyReduceEmissions.class);
+                    intent.putExtra(NATURAL, naturalGas);
+                    intent.putExtra(ELECTRICITY, electricity);
+                    intent.putExtra(FUELOIL, fuelOil);
+                    intent.putExtra(PROPANE, propane);
+                    startActivity(intent);
+                }
+                else if(primaryHeat.equals("Select an Option")){
+                    Toast.makeText(HomeEnergy.this, "You need to select a primary heat source", Toast.LENGTH_LONG).show();
+                }
+
 
             }
         });
+
+    }
+    private void primaryHeatSource(){
+        ArrayAdapter<String> primaryHeatAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.Utility));
+        primaryHeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        primaryHeatSpinner.setAdapter(primaryHeatAdapter);
     }
 
-    private void showToast(String text){
-        Toast.makeText(HomeEnergy.this, text, Toast.LENGTH_LONG).show();
-    }
+
+
 }
