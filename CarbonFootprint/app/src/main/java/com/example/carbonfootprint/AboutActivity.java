118761dd93091ca6de900e62c21c3c7419081b3b
John Beltran <johnbeltran4561@gmail.com>
2021-12-14 18:35:40 -0500

    Alot of work

--- CarbonFootprint/app/src/main/java/com/example/carbonfootprint/AboutActivity.java
+++ CarbonFootprint/app/src/main/java/com/example/carbonfootprint/AboutActivity.java
@@ -2,7 +2,9 @@ package com.example.carbonfootprint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -14,7 +16,17 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         aboutTextView = findViewById(R.id.aboutTextView);
-
-
+        setupHyperlink();
     }
+    private void setupHyperlink()
+    {
+        TextView linkTextView = findViewById(R.id.sourceLink);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        linkTextView.setLinkTextColor(Color.BLACK);
+
+        TextView linkTextView2 = findViewById(R.id.gitLink);
+        linkTextView2.setMovementMethod(LinkMovementMethod.getInstance());
+        linkTextView2.setLinkTextColor(Color.BLACK);
+    }
+
 }
